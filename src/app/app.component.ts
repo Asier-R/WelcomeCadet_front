3d95d45b78d860dad2527d7c1956ae297c50ba21
asier.virtualbox <asier.virtualbox@prueba.com>
2023-08-17 12:53:07 +0200

    Re-estructuración de proyecto front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular-welcomecadet';
+  title = 'WelcomeCadet_front';
 }
