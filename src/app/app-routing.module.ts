23af4cb1a2b3afc494eff8878ed3dbc9ef5895b1
Ander-RL <anderrodriguez205@gmail.com>
2023-08-18 09:02:05 +0200

    Movido contenido de app.component a main.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/app', pathMatch: 'full' },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'app', component: AppComponent }
+  { path: 'main', component: MainComponent }
 ];
 
 @NgModule({
   imports: [
+    BrowserModule,
     RouterModule.forRoot(
       routes,
       { enableTracing: true } // <-- debugging purposes only
