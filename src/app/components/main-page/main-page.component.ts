f74baa42cf3029c75c837abbc1badd3afe5d92d5
Ander-RL <anderrodriguez205@gmail.com>
2024-02-28 18:29:19 +0100

    Http request data extraction

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -11,6 +11,10 @@ export class MainPageComponent {
 
   totalAngularPackages: any;
 
+  userHandle: string = "";
+  userImage: string = "";
+  userBio: string = "";
+
   constructor(private http: HttpClient) { }
 
   onUserSearch() {
@@ -18,9 +22,13 @@ export class MainPageComponent {
     // Simple GET request with response type <any>
     this.http.get<any>('https://api.starcitizen-api.com/b5e9ed2dc66bb3f09c9c85730e48ea63/v1/auto/user/Isaac_Peral').subscribe(data => {
       this.totalAngularPackages = data.total;
-      console.log("[LOG][ngOnInit] Inside subcription", data);
-      window.alert(JSON.stringify(data));
+      this.userHandle = data.data.profile.handle;
+      this.userImage = data.data.profile.image;
+      this.userBio = data.data.profile.bio;
+      console.log("[LOG][ngOnInit] Inside subcription", JSON.stringify(data));
+      //window.alert(JSON.stringify(data));
     })
 
   }
+
 }
