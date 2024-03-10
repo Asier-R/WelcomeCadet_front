594d85210b56f8275fd8f449a0d886b550bfc7e8
Ander-RL <anderrodriguez205@gmail.com>
2024-03-10 12:31:49 +0100

    Dropdown selection two way binding

--- src/app/components/search-page/search-page.component.ts
+++ src/app/components/search-page/search-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import {NgFor} from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.css'
 })
@@ -16,20 +17,27 @@ export class SearchPageComponent {
   userImage: string = "";
   userBio: string = "";
 
+  items: string[] = ["User", "Ship", "Organization"];
+
+  selectedSearch: string = "User";
+
   constructor(private http: HttpClient) { }
 
   onUserSearch() {
-    console.log("[LOG][ngOnInit] Initiating HTTP Request");
+    console.log("[LOG][onUserSearch] Initiating HTTP Request");
     // Simple GET request with response type <any>
     this.http.get<any>('https://api.starcitizen-api.com/b5e9ed2dc66bb3f09c9c85730e48ea63/v1/auto/user/Isaac_Peral').subscribe(data => {
       this.totalAngularPackages = data.total;
       this.userHandle = data.data.profile.handle;
       this.userImage = data.data.profile.image;
       this.userBio = data.data.profile.bio;
-      console.log("[LOG][ngOnInit] Inside subcription", JSON.stringify(data));
-      //window.alert(JSON.stringify(data));
+      console.log("[LOG][onUserSearch] Inside subcription", JSON.stringify(data));
     })
 
+  }
+
+  setSelectedSearch(item: string) {
+    this.selectedSearch = item;
   }
 
 }
